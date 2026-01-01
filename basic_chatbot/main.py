d33512de01d56fab5c13e67fbfb96d8fa7a3decd
sadikurSenpai <mdshadikurrahmansheam@gmail.com>
2026-01-01 12:06:47 +0600

    implemented InMemorySaver for persistence

--- basic_chatbot/main.py
+++ basic_chatbot/main.py
@@ -2,8 +2,10 @@ from langgraph.graph import StateGraph, START, END
 from state import Chat
 from nodes import send_message
 from langchain_core.messages import HumanMessage
-
+from langgraph.checkpoint.memory import InMemorySaver
 chatbot = StateGraph(Chat)
+
+checkpointer = InMemorySaver()
 
 # adding nodes
 chatbot.add_node('send_message', send_message)
@@ -13,11 +15,27 @@ chatbot.add_edge(START, 'send_message')
 chatbot.add_edge('send_message', END)
 
 # compile
-workflow = chatbot.compile()
+workflow = chatbot.compile(checkpointer=checkpointer)
 
-# execution
-result = workflow.invoke({
-    'messages': HumanMessage(content="Hi there!")
-})
 
-print(result['messages'][-1].content)
+# chatting
+thread_id = 'sadik'
+config = {
+        'configurable': {
+            'thread_id': thread_id
+        }
+    }
+while True:
+    user_message = input("User: ")
+    if user_message.strip().lower() in ['quit', 'bye', 'exit']:
+        break
+    
+    user_message = HumanMessage(content=user_message)
+    result = workflow.invoke({
+        'messages': [user_message],
+    }, config=config)
+
+    print(f"AI: {result['messages'][-1].content}")
+
+
+print(workflow.get_state(config))
